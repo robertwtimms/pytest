5d037cb706fd03e8d5e177154f5fb82056e1a395
robertwtimms <robertwtimms@gmail.com>
2018-04-04 14:58:09 +0100

    Changes to be committed: 	modified:   sin.py

--- sin.py
+++ sin.py
@@ -9,7 +9,7 @@ ax1 = fig.add_subplot(211)
 ax1.plot(t, sin(2*pi*t))
 ax1.grid(True)
 ax1.set_ylim((-2, 2))
-ax1.set_ylabel('1 Hz')
+ax1.set_ylabel('f(t)')
 ax1.set_title('A sine wave or two')
 
 for label in ax1.get_xticklabels():
@@ -20,7 +20,8 @@ ax2 = fig.add_subplot(212)
 ax2.plot(t, sin(2*2*pi*t))
 ax2.grid(True)
 ax2.set_ylim((-2, 2))
-l = ax2.set_xlabel('Hi mom')
+ax2.set_ylabel('g(t)')
+l = ax2.set_xlabel('t')
 l.set_color('g')
 l.set_fontsize('large')
 
